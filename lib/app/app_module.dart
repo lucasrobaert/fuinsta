969272c27cbe2a819b3ea1eb6d76db6898aa28e1
Lucas Roabert <lucasrobaert@hotmail.com>
2021-06-17 20:06:57 -0300

    sharedpreference

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
+
+  SharedPreferences _sharedPreferences;
+  AppModule(this._sharedPreferences);
+
   @override
-  final List<Bind> binds = [];
+   List<Bind> get binds => [
+    Bind.singleton((i) => _sharedPreferences)
+  ];
 
   @override
   final List<ModularRoute> routes = [
